698f2582e06228c81c2869578a4b9de4d10864c4
SAY-5 <say.apm35@gmail.com>
2023-12-07 17:12:40 -0500

    in_toto_attestation/v1: fix type hints
    Signed-off-by: SAY-5 <say.apm35@gmail.com>

--- python/in_toto_attestation/v1/resource_descriptor.py
+++ python/in_toto_attestation/v1/resource_descriptor.py
@@ -1,12 +1,23 @@
 #  Wrapper class for in-toto attestation ResourceDescriptor protos.
 
-import in_toto_attestation.v1.resource_descriptor_pb2 as rdpb
-from google.protobuf.struct_pb2 import Value
-import google.protobuf.json_format as pb_json
 import json
 
+import google.protobuf.json_format as pb_json
+import in_toto_attestation.v1.resource_descriptor_pb2 as rdpb
+from google.protobuf.struct_pb2 import Value
+
+
 class ResourceDescriptor:
-    def __init__(self, name: str='', uri: str='', digest: dict=None, content: bytes=bytes(), download_location: str='', media_type: str='', annotations: dict=None) -> None:
+    def __init__(
+        self,
+        name: str = "",
+        uri: str = "",
+        digest: dict = None,
+        content: bytes = bytes(),
+        download_location: str = "",
+        media_type: str = "",
+        annotations: dict = None,
+    ) -> None:
         self.pb = rdpb.ResourceDescriptor()
         self.pb.name = name
         self.pb.uri = uri
@@ -19,12 +30,12 @@ class ResourceDescriptor:
             self.pb.annotations.update(annotations)
 
     @staticmethod
-    def copy_from_pb(proto: type[rdpb.ResourceDescriptor]) -> 'ResourceDescriptor':
+    def copy_from_pb(proto: rdpb.ResourceDescriptor) -> "ResourceDescriptor":
         rd = ResourceDescriptor()
         rd.pb.CopyFrom(proto)
         return rd
 
     def validate(self) -> None:
         # at least one of name, URI or digest are required
-        if self.pb.name == '' and self.pb.uri == '' and len(self.pb.digest) == 0:
+        if self.pb.name == "" and self.pb.uri == "" and len(self.pb.digest) == 0:
             raise ValueError("At least one of name, URI, or digest need to be set")
